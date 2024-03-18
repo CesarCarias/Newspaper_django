6dc83d50fc712d4af7b028312f1df8b8301bf1fd
CesarCarias <cesarleonardocariasvasquez@gmail.com>
2024-03-18 11:06:12 -0400

    password change added

--- accounts/views.py
+++ accounts/views.py
@@ -3,6 +3,7 @@ from django.urls import reverse_lazy
 from django.contrib.auth.forms import UserCreationForm
 from django.views.generic import CreateView
 
+
 # Create your views here.
 class SignUpView(CreateView):
     form_class = UserCreationForm
